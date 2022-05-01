88497b50e453de37b1afe9d18008b0170739d203
mborreros <mlbsindia@gmail.com>
2022-05-01 12:51:24 -0400

    commit tree bdc6af4cf857f8f6a4406e1f193a2882ab85b1fb

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,19 @@
 class StudentsController < ApplicationController
 
   def index
+    if params.has_key?(:name)
+      #find record by name
+      found_student = Student.where(last_name: params[:name].capitalize)
+      render json: found_student
+    else 
     students = Student.all
     render json: students
+    end
+  end
+
+  def show
+    this_student = Student.find(params[:id])
+    render json: this_student
   end
 
 end
